7a8b79d37e07a45414f9f1724d2964e9c62cba7a
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-29 01:18:12 +0330

    coin component added and search box completed.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,15 @@
 import React , { useEffect , useState } from 'react';
 import { getCoin } from "../services/api";
 import Load from "./Load";
+import Coin from "./Coin";
 
 const Landing = () => {
     const [ coins , setCoins ] = useState ( [] );
+    const [ search , setSearch ] = useState ( "" );
+    const searchHandler = ( event ) => {
+        setSearch ( event.target.value );
+    }
+    const searchedCoins = coins.filter ( coin => coin.name.toLowerCase ().includes ( search.toLowerCase () ) )
     useEffect ( () => {
         const fetchAPI = async () => {
             const data = await getCoin ();
@@ -14,12 +20,18 @@ const Landing = () => {
     } , [] )
     return (
         <>
-            <input type="text" placeholder="Search"/>
+            <input type="text" placeholder="Search" value={ search } onChange={ searchHandler }/>
             {
                 coins.length ?
                     <div>
                         {
-                            coins.map ( coin => <p key={ coin.id }>{ coin.name }</p> )
+                            searchedCoins.map ( coin => <Coin
+                                key={ coin.id }
+                                name={ coin.name }
+                                image={ coin.image }
+                                symbol={ coin.symbol } price={ coin.current_price }
+                                marketCap={ coin.market_cap }
+                                priceChange={ coin.price_change_percentage_24h }/> )
                         }
                     </div>
                     :
