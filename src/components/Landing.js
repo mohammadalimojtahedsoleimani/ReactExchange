23b93543263a744816b379b574f29a991632482e
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-29 01:41:08 +0330

    styles added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React , { useEffect , useState } from 'react';
 import { getCoin } from "../services/api";
 import Load from "./Load";
 import Coin from "./Coin";
+import styles from "./Landing.module.css"
 
 const Landing = () => {
     const [ coins , setCoins ] = useState ( [] );
@@ -20,22 +21,24 @@ const Landing = () => {
     } , [] )
     return (
         <>
-            <input type="text" placeholder="Search" value={ search } onChange={ searchHandler }/>
+            <input className={styles.input} type="text" placeholder="Search" value={search} onChange={searchHandler} />
             {
                 coins.length ?
-                    <div>
+                    <div className={styles.coinContainer}>
                         {
-                            searchedCoins.map ( coin => <Coin
-                                key={ coin.id }
-                                name={ coin.name }
-                                image={ coin.image }
-                                symbol={ coin.symbol } price={ coin.current_price }
-                                marketCap={ coin.market_cap }
-                                priceChange={ coin.price_change_percentage_24h }/> )
+                            searchedCoins.map(coin => <Coin
+                                key={coin.id}
+                                name={coin.name}
+                                image={coin.image}
+                                symbol={coin.symbol}
+                                price={coin.current_price}
+                                marketCap={coin.market_cap}
+                                priceChange={coin.price_change_percentage_24h}
+                            />)
                         }
-                    </div>
-                    :
-                    <Load/>
+                    </div> :
+
+                    <Load />
             }
 
         </>
